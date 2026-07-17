6aa9a3c8d047f7bfddf2666184016f2ecf0b8e68
Ryan <Woodedcode@gmail.com>
2026-07-17 12:29:26 -0500

    Add files via upload

---
+++ user.js
@@ -0,0 +1,11 @@
+console.log(localStorage.getItem("id"));
+
+async function main() {
+    const id = localStorage.getItem("id")
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postData = await posts.json();
+
+    console.log(postData)
+}
+
+main()
